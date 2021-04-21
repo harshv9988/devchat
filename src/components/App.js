f96f1b0ebc1d2904d19f478f7320bbb0cbfd9d60
harshv9988 <harshv9988@gmail.com>
2021-04-21 12:34:30 +0530

    logout redux and user panel added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-export class App extends Component {
-  render() {
-    return <div>hello</div>;
-  }
-}
+import { Grid } from "semantic-ui-react";
+
+import ColorPanel from "./colorPanel/ColorPanel";
+import SidePanel from "./sidePanel/SidePanel";
+import Messages from "./messages/Messages";
+import MetaPanel from "./metaPanel/MetaPanel";
+
+const App = () => {
+  return (
+    <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+      <ColorPanel />
+      <SidePanel />
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages />
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
+  );
+};
 
 export default App;
